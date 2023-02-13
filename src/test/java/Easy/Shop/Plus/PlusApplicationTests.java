22586643b2e02971b8aec480bc80840a5187edfe
Simo Mrdjen <dr.sizni@gmail.com>
2023-02-13 18:17:40 +0100

    first commit

---
+++ src/test/java/Easy/Shop/Plus/PlusApplicationTests.java
@@ -0,0 +1,13 @@
+package Easy.Shop.Plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
