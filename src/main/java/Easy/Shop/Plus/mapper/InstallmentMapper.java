c56fb8249a88f8b58d89f7cc5dabaf45b7205610
Simo Mrdjen <dr.sizni@gmail.com>
2023-02-13 21:14:13 +0100

    added mappers

---
+++ src/main/java/Easy/Shop/Plus/mapper/InstallmentMapper.java
@@ -0,0 +1,48 @@
+package Easy.Shop.Plus.mapper;
+
+import Easy.Shop.Plus.dto.InstallmentDto;
+import Easy.Shop.Plus.entity.Installment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InstallmentMapper {
+
+    private final PurchaseContractMapper mapper;
+
+    public Installment mapCreateDtoToEntity(InstallmentDto dto) {
+        return new Installment(
+                mapper.mapCreateInstallmentDtoToEntity(dto.getPurchaseContractDto()),
+                dto.getInstallmentOrdinal(),
+                dto.getInstallmentAmount(),
+                dto.getMaturityDate()
+        );
+    }
+
+    public Installment mapEditDtoToEntity(InstallmentDto dto) {
+        return new Installment(
+                dto.getId(),
+                mapper.mapCreateInstallmentDtoToEntity(dto.getPurchaseContractDto()),
+                dto.getInstallmentOrdinal(),
+                dto.getInstallmentAmount(),
+                dto.getMaturityDate(),
+                dto.getPaidAmount(),
+                dto.getPaymentDate(),
+                dto.getPaymentMethod()
+        );
+    }
+
+    public InstallmentDto mapGetEntityToDto(Installment installment) {
+        return new InstallmentDto(
+                installment.getId(),
+                mapper.mapGetEntityToDto(installment.getPurchaseContract()),
+                installment.getInstallmentOrdinal(),
+                installment.getInstallmentAmount(),
+                installment.getMaturityDate(),
+                installment.getPaidAmount(),
+                installment.getPaymentDate(),
+                installment.getPaymentMethod());
+    }
+}
